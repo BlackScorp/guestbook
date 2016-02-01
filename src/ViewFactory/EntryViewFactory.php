0eaeaccff09aad09a6529b45217b67b115bcb929
BlackScorp <vitalij@blackscorp.de>
2016-02-01 21:47:10 +0100

    added entry repository and entry view factory interfaces

---
+++ src/ViewFactory/EntryViewFactory.php
@@ -0,0 +1,14 @@
+<?php
+namespace BlackScorp\GuestBook\ViewFactory;
+
+use BlackScorp\GuestBook\Entity\EntryEntity;
+use BlackScorp\GuestBook\View\EntryView;
+
+interface EntryViewFactory
+{
+    /**
+     * @param EntryEntity $entity
+     * @return EntryView
+     */
+    public function create(EntryEntity $entity);
+}
