cec56e82f787944086fa392074dde32f24d9973e
BlackScorp <vitalij@blackscorp.de>
2016-02-01 21:26:44 +0100

    create failing test EntriesNotExists

--- tests/UseCase/ListEntriesTest.php
+++ tests/UseCase/ListEntriesTest.php
@@ -5,5 +5,10 @@ class ListEntriesTest extends PHPUnit_Framework_TestCase
 {
     public function testEntriesNotExists()
     {
+        $request = new FakeViewEntriesRequest();
+        $response = new FakeViewEntriesResponse();
+        $useCase = new ViewEntriesUseCase();
+        $useCase->process($request, $response);
+        $this->assertEmpty($response->entries);
     }
 }
